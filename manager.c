33ab93d39ee059333973dce3fb6666c54d2f7330
LeeDonggyu <asasd456123@naver.com>
2021-04-26 00:28:27 +0900

    make loadData function

--- manager.c
+++ manager.c
@@ -57,6 +57,14 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
+	fp = fopen("product.txt","rt");
+	for(int i=0;i<100;i++){
+		fscanf(fp,"%d",&p[i].weight);
+		if(feof(fp)) break;
+		fscanf(fp," %d",&p[i].price);
+		fscanf(fp," %[^\n]s",p[i].name);
+		count++;
+	}
 
 
 
